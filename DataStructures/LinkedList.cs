8e887425cd4a815a606d8f6296c600e979181736
Kyle Galloway <123207266+Kyle-Galloway01@users.noreply.github.com>
2024-03-26 18:02:46 -0500

    Create LinkedList.cs

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,59 @@
+using System;
+
+public class Node<T>
+{
+    public T Data;
+    public Node<T> Next;
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    private Node<T> head;
+
+    public void Add(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node<T> current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void Display()
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " ");
+            current = current.Next;
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Display();
+    }
+}
